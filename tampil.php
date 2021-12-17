cda625c8a06fe59646a60658b76cd4dc292bd883
Anang <85871474+VATEMONK@users.noreply.github.com>
2021-12-17 12:17:37 +0700

    Add files via upload

---
+++ tampil.php
@@ -0,0 +1,21 @@
+<?php
+    include './koneksi.php';
+    echo "<a href='formulir.php'>Tambah data</a><br>";
+
+    $sql = "SELECT * FROM buku";
+    $result = $conn->query($sql);
+    $a = 1;
+
+    while ($baris = mysqli_fetch_array($result)){
+        echo "<br>";
+        echo "$a";
+        echo "<br>";
+        echo "Id Buku : " . $baris[0] . "<br>";
+        echo "Penulis Buku : " . $baris[1] . "<br>";
+        echo "Judul Buku : " . $baris[2] . "<br>";
+        echo "<a href='ubah data.php?buku=$baris[0]'>Ubah &nbsp</a>";
+        echo "<a href='hapus.php?id_buku=$baris[0]'>Hapus</a>";
+        $a++;
+    }
+    $conn->close(); 
+?>
